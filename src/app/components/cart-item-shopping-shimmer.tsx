df55ef15867f8e477c0cf2f20c415467fe68fc72
roniathayde <pontes014@gmail.com>
2024-04-12 23:24:38 -0300

    style: add a shimmer effect component to card

---
+++ src/app/components/cart-item-shopping-shimmer.tsx
@@ -0,0 +1,16 @@
+export function CartItemShoppingShimmer() {
+  return (
+    <div className="flex w-full  flex-col  items-center overflow-hidden rounded-lg  shadow-[0px_2px_8px_0px_rgb(0_0_0_/_13%)] ">
+      <div className="h-[130px] w-full animate-pulse rounded bg-gray-200" />
+
+      <div className="flex w-full flex-col px-4 py-3">
+        <div className="flex w-full flex-col items-center justify-between gap-2 ">
+          <div className="h-3 w-full animate-pulse rounded bg-gray-200" />
+          <div className="h-3 w-full animate-pulse rounded bg-gray-200" />
+          <div className="h-3 w-full animate-pulse rounded bg-gray-200" />
+        </div>
+      </div>
+      <div className="h-3 w-full animate-pulse  bg-gray-200"></div>
+    </div>
+  )
+}
