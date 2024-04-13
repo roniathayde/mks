c599e4c590d345c4b68ddedf66e990e6872a5a0c
roniathayde <pontes014@gmail.com>
2024-04-12 23:25:41 -0300

    fix: add a config to next.config.mjs to accept a Image component src with webp image of other url

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mks-sistemas.nyc3.digitaloceanspaces.com',
+        port: '',
+        pathname: '/products/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
